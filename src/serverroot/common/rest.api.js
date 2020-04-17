59b67220cd39a67ea301f2bc8b62b70d084959a5
ZheyangSong <jerome_soung@hotmail.com>
2020-04-17 12:13:31 -0700

    Treat non-200 Response as Error for Propagation
    When the app server receives non-200 response from other servcies,
    the app server should treat the response as error and forward the
    resonse as is (with the error code) to its client.
    
    closes-jira-bug: CEM-14826
    Change-Id: Ib77d351fef298311166219e3f19dc7c2ee8c94a8

--- src/serverroot/common/rest.api.js
+++ src/serverroot/common/rest.api.js
@@ -158,17 +158,23 @@ APIServer.prototype.makeHttpsRestCall = function (options, callback)
             result += chunk;
         });
         res.on('end', function () {
-            callback(null, result, res);
+            var statusCode = res.statusCode;
+
+            if (statusCode >= 400 && statusCode < 500) {
+                errorback(result, null, res);
+            } else {
+                callback(null, result, res);
+            }
         });
         res.on('error', function (err) {
-            callback(err);
+            errorback(err, res, callback);
         })
     });
 
     // req error
     req.on('error', function (err) {
         logutils.logger.error(err.stack);
-        callback(err);
+        errorback(err, res, callback);
     });
 
     //send request with the postData form
