bd82e304a611e9b9dde8f7ed9179e03a8508726c
Priyanka Pasupuleti <ppasupuleti@juniper.net>
2021-10-21 23:59:48 -0700

    Fix Title : [ CEM-24393 ] Unable to delete the Security group rule vi tungsten fabric webui Problem statement: Unable to delete the Security group rule vi tungsten fabric webui Proposed fix: Increasing payload Limit in webserverstart.js Testing done Closes-Jira-Bug [CEM-24393] Branches where the commit is being checked in master and R2011
    Signed-off-by: Priyanka Pasupuleti <ppasupuleti@juniper.net>
    Change-Id: I9a76dd83c9a8dab78fa9161c85f67eaf998e1507

--- webServerStart.js
+++ webServerStart.js
@@ -148,8 +148,8 @@ function initializeAppConfig (appObj, routesRegister)
 
         next();
     });
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json({ limit: "50mb" }));
+    app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
     routesRegister(app);
     // Catch-all error handler
     app.use(function (err, req, res, next) {
